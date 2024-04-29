04004effd4d397d303491cb4ee2acba1177cb809
wagako3 <cjporteria@gmail.com>
2024-04-30 02:09:33 +0800

    add social media icons

--- frontend/src/components/contactMe.tsx
+++ frontend/src/components/contactMe.tsx
@@ -5,13 +5,24 @@ export default function ContactMe() {
     <div className='contactMe--section'>
       <h1>Contact Me</h1>
       <div className='contactMe--socials'>
-        socials
+        <div className='socialBox'>
+          <img className='socialIcon' src="../src/files/facebook.png" alt="" />
+        </div>
+        <div className='socialBox'>
+          <img className='socialIcon' src="../src/files/linkedin.png" alt="" />
+        </div>
+        <div className='socialBox'>
+          <img className='socialIcon' src="../src/files/instagram.png" alt="" />
+        </div>
+        <div className='socialBox'>
+          <img className='socialIcon' src="../src/files/email.png" alt="" />
+        </div>
       </div>
       <div className='contactMe--form'>
         <p>Leave a message</p>
         <input className='contactMe--name' type="text" placeholder='Name/Organization' />
-        <input className='contactMe--email' type="email" placeholder='email'/>
-        <input className='contactMe--message' type="text" placeholder='message'/>
+        <input className='contactMe--email' type="email" placeholder='Email'/>
+        <input className='contactMe--message' type="text" placeholder='Message'/>
         <button className='submit'>Send</button>
       </div>
     </div>
