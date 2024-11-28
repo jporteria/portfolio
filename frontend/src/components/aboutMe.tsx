fff71f64947d4be4ec5fe9f2a0833525260ead86
jporteria <cjporteria@gmail.com>
2024-11-29 07:29:00 +0800

    test deploy

--- frontend/src/components/aboutMe.tsx
+++ frontend/src/components/aboutMe.tsx
@@ -1,49 +1,49 @@
-import { useInView } from 'react-intersection-observer'
-import Skills from './skills'
-import { useContext } from 'react'
-import { Context } from '../app'
+import { useInView } from "react-intersection-observer";
+import Skills from "./skills";
+import { useContext } from "react";
+import { Context } from "../app";
 
 export default function AboutMe() {
-
-  const { setAboutBox } = useContext(Context)
+  const { setAboutBox } = useContext(Context);
 
   const { ref, inView } = useInView({
-      threshold: 0.3
-  })
-  
+    threshold: 0.3,
+  });
+
   // const aboutMe = document.getElementById("aboutMe--section")
   // if(entry?.isIntersecting){
   //   aboutMe?.classList.add("nav--active")
   // }
-  function showAboutBox(){
-      setAboutBox(true)
+  function showAboutBox() {
+    setAboutBox(true);
   }
 
   return (
-    <section className='aboutMe--section' id='aboutMe--section' ref={ref}>
-      <h1 className={inView ? 'showHidden' : 'hidden'}>About Me</h1>
-      <div className={inView ? 'aboutMe showHidden' : 'aboutMe hidden'}>
-          <div className='aboutMePhotoContainer'>
-            <img className='aboutMePhoto' src="images/avatar.png" alt="photo not available" />
-          </div>
-          <div className='summary'>
-            <p>
-              I'm a skilled Full Stack Developer with
-              expertise in front-end and back-end
-              development. I'm passionate about
-              creating and optimizing websites. My
-              aptitude for quickly grasping new
-              concepts, along with my collaborative
-              approach, makes me an asset to any
-              professional team. I'm prepared to
-              enhance your development team with
-              innovative solutions and unwavering
-              dedication.
-            </p>
-            <button className='more' onClick={showAboutBox}>More about me</button>
-          </div>
+    <section className="aboutMe--section" id="aboutMe--section" ref={ref}>
+      <h1 className={inView ? "showHidden" : "hidden"}>About Me</h1>
+      <div className={inView ? "aboutMe showHidden" : "aboutMe hidden"}>
+        <div className="aboutMePhotoContainer">
+          <img
+            className="aboutMePhoto"
+            src="images/avatar.png"
+            alt="photo not available"
+          />
+        </div>
+        <div className="summary">
+          <p>
+            I'm a skilled Full Stack Developer with expertise in front-end and
+            back-end development. I'm passionate about creating and optimizing
+            websites. My aptitude for quickly grasping new concepts, along with
+            my collaborative approach, makes me an asset to any professional
+            team. I'm prepared to enhance your development team with innovative
+            solutions and unwavering dedication.
+          </p>
+          <button className="more" onClick={showAboutBox}>
+            More about me
+          </button>
+        </div>
       </div>
       <Skills />
     </section>
-  )
+  );
 }
