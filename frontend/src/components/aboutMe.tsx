4687bcff5bdec747e1f7bf3fdbd7e870825ff9b2
jporteria <cjporteria@gmail.com>
2025-04-01 21:27:47 +0800

    fixed ui

--- frontend/src/components/aboutMe.tsx
+++ frontend/src/components/aboutMe.tsx
@@ -33,6 +33,7 @@ export default function AboutMe() {
           <p>
           I am a dedicated Full Stack Developer with experience in both front-end and back-end development. Passionate about building and optimizing web applications, I thrive on creating seamless user experiences and solving real-world problems.
           <br />
+          <br />
           I take pride in my ability to learn quickly, adapt to new technologies, and collaborate effectively within a team. Eager to contribute my skills and creativity, I’m ready to grow in a dynamic development environment.
           </p>
           <button className="more" onClick={showAboutBox} onBlur={() => setAboutBox(false)}>
